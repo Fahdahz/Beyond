144f29dc02669b73ffdd90c26b675bdc8272e908
Fahdahz <Fahdah.zms@gmail.com>
2025-09-28 12:32:19 +0300

    creating beyond projct

---
+++ Beyond/Beyond/BeyondApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeyondApp.swift
+//  Beyond
+//
+//  Created by Fahdah Alsamari on 06/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct BeyondApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
