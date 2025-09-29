8e169743342888908eb7491c275354f7b032d10b
Ghadeer Fallatah <gfallatah25@twq.idserve.net>
2025-09-29 09:12:20 +0300

    Create new project folder to replace the corrupted one.

---
+++ Beyond_challenge/Beyond_challenge/Beyond_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Beyond_challengeApp.swift
+//  Beyond_challenge
+//
+//  Created by Ghadeer Fallatah on 07/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct Beyond_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
